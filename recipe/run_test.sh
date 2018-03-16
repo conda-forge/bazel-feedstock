d3e0442ef5586cfd254545ae9008af998985c005
Jonathan Helmus <jjhelmus@gmail.com>
2018-03-16 12:31:16 -0500

    bazel: update to 0.11.1

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,8 +1,3 @@
-# Move the build environment's lib directory to prevent DSOs 
-# present there from being used by the bazel binaries. 
-# see: https://github.com/conda/conda-build/issues/2625
-mv ${BUILD_PREFIX}/lib ${BUILD_PREFIX}/lib_moved
-
 # Test by building an example from the tutorial.
 # https://github.com/bazelbuild/examples/
 # https://docs.bazel.build/versions/master/tutorial/cpp.html
