bb2eb1184c77ad602dc2eea63cea90d7f6391e4a
rluria14 <ryanlur14@gmail.com>
2020-02-06 17:11:19 -0500

    try removing missing_dso_whitelist

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 set -v -x
-./compile.sh
-mkdir -p $PREFIX/bin/
-cp output/bazel $PREFIX/bin/
+
+if [ $(uname) == Darwin ]; then
+    if [[ $(basename $CONDA_BUILD_SYSROOT) != "MacOSX10.12.sdk" ]]; then
+      echo "WARNING: You asked me to use $CONDA_BUILD_SYSROOT as the MacOS SDK"
+      echo "         But because of the use of Objective-C Generics we need at"
+      echo "         least MacOSX10.12.sdk"
+      CONDA_BUILD_SYSROOT=/opt/MacOSX10.12.sdk
+      if [[ ! -d $CONDA_BUILD_SYSROOT ]]; then
+        echo "ERROR: $CONDA_BUILD_SYSROOT is not a directory"
+        exit 1
+      fi
+    fi
+    ./compile.sh
+    mkdir -p $PREFIX/bin/
+    mv output/bazel $PREFIX/bin
+else
+    ./compile.sh
+    mkdir -p $PREFIX/bin/
+    mv output/bazel $PREFIX/bin
+fi
