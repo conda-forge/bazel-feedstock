26ac2cea6b1fc616612a5cd419a722776904079a
Uwe L. Korn <uwe.korn@quantco.com>
2021-03-25 12:15:42 +0100

    Extract as much logic as possible into the toolchain

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,44 +2,23 @@
 
 set -euxo pipefail
 
-if [[ "${target_platform}" == "osx-64" ]]; then
-  TARGET_CPU="darwin_x86_64"
+if [[ "${target_platform}" == osx-64 ]]; then
+  export LDFLAGS="${LDFLAGS} -framework IOKit"
 elif [[ "${target_platform}" == "osx-arm64" ]]; then
-  TARGET_CPU="darwin_arm64"
-elif [[ "${target_platform}" == "linux-64" ]]; then
-  TARGET_CPU="k8"
-elif [[ "${target_platform}" == "linux-aarch64" ]]; then
-  TARGET_CPU="aarch64"
-elif [[ "${target_platform}" == "linux-ppc64le" ]]; then
-  TARGET_CPU="ppc"
-fi
-if [[ "${build_platform}" == "osx-64" ]]; then
-  BUILD_CPU="darwin"
-elif [[ "${build_platform}" == "osx-arm64" ]]; then
-  BUILD_CPU="darwin"
-elif [[ "${build_platform}" == "linux-64" ]]; then
-  BUILD_CPU="k8"
-elif [[ "${build_platform}" == "linux-aarch64" ]]; then
-  BUILD_CPU="aarch64"
-elif [[ "${build_platform}" == "linux-ppc64le" ]]; then
-  BUILD_CPU="ppc"
-fi
-# The current Bazel release cannot distinguish between osx-arm64 and osx-64.
-# This will change with later releases and then we should get rid of this section again.
-if [[ "${target_platform}" == osx-* ]]; then
-  if [[ "${build_platform}" == "${target_platform}" ]]; then
-    TARGET_CPU="darwin"
-    BUILD_CPU="darwin"
-  fi
+  export LDFLAGS="${LDFLAGS} -framework IOKit -mmacosx-version-min=11.0"
+else
+  export LDFLAGS="${LDFLAGS} -lpthread"
 fi
 
-export BAZEL_USE_CPP_ONLY_TOOLCHAIN=1
+# Generate toolchain and set necessary environment variables
+source ${RECIPE_DIR}/gen-bazel-toolchain.sh
+
 # For debugging purposes, you can add
 # --logging=6 --subcommands --verbose_failures
 # This is though too much log output for Travis CI.
 export BAZEL_BUILD_OPTS="--crosstool_top=//custom_toolchain:toolchain --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include --cpu=${TARGET_CPU}"
 export EXTRA_BAZEL_ARGS="--host_javabase=@local_jdk//:jdk"
-
+sed -ie "s:\${INSTALL_NAME_TOOL}:${INSTALL_NAME_TOOL:-install_name_tool}:" src/BUILD
 sed -ie "s:\${PREFIX}:${PREFIX}:" src/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/grpc/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/systemlibs/protobuf.BUILD
@@ -47,17 +26,9 @@ sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/ijar/BUILD
 sed -ie "s:TARGET_CPU:${TARGET_CPU}:" compile.sh
 sed -ie "s:BUILD_CPU:${BUILD_CPU}:" compile.sh
 
+./compile.sh
+
 if [[ "${target_platform}" == osx-* ]]; then
-  if [[ "${target_platform}" == "osx-64" ]]; then
-    export LDFLAGS="${LDFLAGS} -framework IOKit"
-  elif [[ "${target_platform}" == "osx-arm64" ]]; then
-    export LDFLAGS="${LDFLAGS} -framework IOKit -mmacosx-version-min=11.0"
-  fi
-
-  sed -ie "s:\${INSTALL_NAME_TOOL}:${INSTALL_NAME_TOOL}:" src/BUILD
-  ${RECIPE_DIR}/gen-bazel-toolchain.sh
-
-  ./compile.sh
   mkdir -p $PREFIX/bin/
   cp ${RECIPE_DIR}/bazel-osx-wrapper.sh $PREFIX/bin/bazel
   chmod +x $PREFIX/bin/bazel
@@ -77,9 +48,6 @@ if [[ "${target_platform}" == osx-* ]]; then
   find $PREFIX/share/bazel/install/${INSTALL_BASE_KEY} -type f | xargs touch -mt $(($(date '+%Y') + 10))10101010
   chmod -R a-w $PREFIX/share/bazel/install/${INSTALL_BASE_KEY}
 else
-  export LDFLAGS="${LDFLAGS} -lpthread"
-  ${RECIPE_DIR}/gen-bazel-toolchain.sh
-  ./compile.sh
   mkdir -p $PREFIX/bin/
   mv output/bazel $PREFIX/bin
 fi
--- recipe/gen-bazel-toolchain.sh
+++ recipe/gen-bazel-toolchain.sh
@@ -8,8 +8,8 @@ function apply_cc_template() {
   sed -ie "s:TARGET_SYSTEM:${TARGET_SYSTEM}:" $1
   sed -ie "s:TARGET_PLATFORM:${target_platform}:" $1
   sed -ie "s:\${CONDA_BUILD_SYSROOT}:${CONDA_BUILD_SYSROOT}:" $1
-  sed -ie "s:\${COMPILER_VERSION}:${COMPILER_VERSION:-}:" $1
-  sed -ie "s:\${GCC}:${GCC}:" $1
+  sed -ie "s:\${COMPILER_VERSION}:${BAZEL_TOOLCHAIN_COMPILER_VERSION:-}:" $1
+  sed -ie "s:\${GCC}:${BAZEL_TOOLCHAIN_GCC}:" $1
   sed -ie "s:\${PREFIX}:${PREFIX}:" $1
   sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" $1
   sed -ie "s:\${LD}:${LD}:" $1
@@ -19,16 +19,18 @@ function apply_cc_template() {
   sed -ie "s:\${LDFLAGS}:${LDFLAGS}:" $1
   sed -ie "s:\${NM}:${NM}:" $1
   sed -ie "s:\${STRIP}:${STRIP}:" $1
-  sed -ie "s:\${AR}:${AR}:" $1
+  sed -ie "s:\${AR}:${BAZEL_TOOLCHAIN_AR}:" $1
   sed -ie "s:\${HOST}:${HOST}:" $1
-  sed -ie "s:\${LIBCXX}:${LIBCXX}:" $1
+  sed -ie "s:\${LIBCXX}:${BAZEL_TOOLCHAIN_LIBCXX}:" $1
 }
+
+export BAZEL_USE_CPP_ONLY_TOOLCHAIN=1
 
 # set up bazel config file for conda provided clang toolchain
 cp -r ${RECIPE_DIR}/custom_toolchain .
 pushd custom_toolchain
   if [[ "${target_platform}" == osx-* ]]; then
-    export COMPILER_VERSION=$($CC -v 2>&1 | head -n1 | cut -d' ' -f3)
+    export BAZEL_TOOLCHAIN_COMPILER_VERSION=$($CC -v 2>&1 | head -n1 | cut -d' ' -f3)
     sed -e "s:\${CLANG}:${CLANG}:" \
         -e "s:\${INSTALL_NAME_TOOL}:${INSTALL_NAME_TOOL}:" \
         -e "s:\${CONDA_BUILD_SYSROOT}:${CONDA_BUILD_SYSROOT}:" \
@@ -39,56 +41,56 @@ pushd custom_toolchain
         -e "s:\${CONDA_BUILD_SYSROOT}:${CONDA_BUILD_SYSROOT}:" \
         cc_wrapper.sh.template > cc_wrapper_build.sh
     chmod +x cc_wrapper.sh
-    export GCC="cc_wrapper.sh"
-    export GCC="cc_wrapper.sh"
-    export LIBCXX="c++"
-    export AR=${LIBTOOL}
+    export BAZEL_TOOLCHAIN_GCC="cc_wrapper.sh"
+    export BAZEL_TOOLCHAIN_LIBCXX="c++"
+    export BAZEL_TOOLCHAIN_AR=${LIBTOOL}
   else
-    export COMPILER_VERSION=$(${CC} -v 2>&1|tail -n1|cut -d' ' -f3)
-    export AR=$(basename ${AR})
+    export BAZEL_TOOLCHAIN_COMPILER_VERSION=$(${CC} -v 2>&1|tail -n1|cut -d' ' -f3)
+    export BAZEL_TOOLCHAIN_AR=$(basename ${AR})
     touch cc_wrapper.sh
-    export LIBCXX="stdc++"
+    export BAZEL_TOOLCHAIN_LIBCXX="stdc++"
+    export BAZEL_TOOLCHAIN_GCC="${GCC}"
   fi
 
-  TARGET_SYSTEM="${HOST}"
+  export TARGET_SYSTEM="${HOST}"
   if [[ "${target_platform}" == "osx-64" ]]; then
-    TARGET_LIBC="macosx"
-    TARGET_CPU="darwin_x86_64"
-    TARGET_SYSTEM="x86_64-apple-macosx"
+    export TARGET_LIBC="macosx"
+    export TARGET_CPU="darwin_x86_64"
+    export TARGET_SYSTEM="x86_64-apple-macosx"
   elif [[ "${target_platform}" == "osx-arm64" ]]; then
-    TARGET_LIBC="macosx"
-    TARGET_CPU="darwin_arm64"
-    TARGET_SYSTEM="arm64-apple-macosx"
+    export TARGET_LIBC="macosx"
+    export TARGET_CPU="darwin_arm64"
+    export TARGET_SYSTEM="arm64-apple-macosx"
   elif [[ "${target_platform}" == "linux-64" ]]; then
-    TARGET_LIBC="unknown"
-    TARGET_CPU="k8"
+    export TARGET_LIBC="unknown"
+    export TARGET_CPU="k8"
   elif [[ "${target_platform}" == "linux-aarch64" ]]; then
-    TARGET_LIBC="unknown"
-    TARGET_CPU="aarch64"
+    export TARGET_LIBC="unknown"
+    export TARGET_CPU="aarch64"
   elif [[ "${target_platform}" == "linux-ppc64le" ]]; then
-    TARGET_LIBC="unknown"
-    TARGET_CPU="ppc"
+    export TARGET_LIBC="unknown"
+    export TARGET_CPU="ppc"
   fi
-  BUILD_SYSTEM=${BUILD}
+  export BUILD_SYSTEM=${BUILD}
   if [[ "${build_platform}" == "osx-64" ]]; then
-    BUILD_CPU="darwin"
-    BUILD_SYSTEM="x86_64-apple-macosx"
+    export BUILD_CPU="darwin"
+    export BUILD_SYSTEM="x86_64-apple-macosx"
   elif [[ "${build_platform}" == "osx-arm64" ]]; then
-    BUILD_CPU="darwin"
-    BUILD_SYSTEM="arm64-apple-macosx"
+    export BUILD_CPU="darwin"
+    export BUILD_SYSTEM="arm64-apple-macosx"
   elif [[ "${build_platform}" == "linux-64" ]]; then
-    BUILD_CPU="k8"
+    export BUILD_CPU="k8"
   elif [[ "${build_platform}" == "linux-aarch64" ]]; then
-    BUILD_CPU="aarch64"
+    export BUILD_CPU="aarch64"
   elif [[ "${build_platform}" == "linux-ppc64le" ]]; then
-    BUILD_CPU="ppc"
+    export BUILD_CPU="ppc"
   fi
   # The current Bazel release cannot distinguish between osx-arm64 and osx-64.
   # This will change with later releases and then we should get rid of this section again.
   if [[ "${target_platform}" == osx-* ]]; then
     if [[ "${build_platform}" == "${target_platform}" ]]; then
-      TARGET_CPU="darwin"
-      BUILD_CPU="darwin"
+      export TARGET_CPU="darwin"
+      export BUILD_CPU="darwin"
     fi
   fi
   
@@ -100,13 +102,13 @@ pushd custom_toolchain
   (
     if [[ "${build_platform}" != "${target_platform}" ]]; then
       if [[ "${target_platform}" == osx-* ]]; then
-        GCC=cc_wrapper_build.sh
+        BAZEL_TOOLCHAIN_GCC=cc_wrapper_build.sh
       else
-        GCC=${GCC//${HOST}/${BUILD}}
+        BAZEL_TOOLCHAIN_GCC=${BAZEL_TOOLCHAIN_GCC//${HOST}/${BUILD}}
       fi
       TARGET_CPU=${BUILD_CPU}
       TARGET_SYSTEM=${BUILD_SYSTEM}
-      TARGET_PLATFORM=${build_platform}
+      target_platform=${build_platform}
       PREFIX=${BUILD_PREFIX}
       LD=${LD//${HOST}/${BUILD}}
       CFLAGS=${CFLAGS//${PREFIX}/${BUILD_PREFIX}}
@@ -115,7 +117,7 @@ pushd custom_toolchain
       LDFLAGS=${LDFLAGS//${PREFIX}/${BUILD_PREFIX}}
       NM=${NM//${HOST}/${BUILD}}
       STRIP=${STRIP//${HOST}/${BUILD}}
-      AR=${AR//${HOST}/${BUILD}}
+      BAZEL_TOOLCHAIN_AR=${BAZEL_TOOLCHAIN_AR//${HOST}/${BUILD}}
       HOST=${BUILD}
     fi
     apply_cc_template cc_toolchain_build_config.bzl
