49b83e8192d36619426ce778fd37080e7a6be3a0
Nehal J Wani <nehaljw.kkd1@gmail.com>
2021-12-09 22:01:59 -0500

    Silence chdir in bazel wrapper

--- recipe/bazel-wrapper.sh
+++ recipe/bazel-wrapper.sh
@@ -5,9 +5,9 @@ PREFIX_DIR=$(dirname ${BASH_SOURCE})
 if [[ $(uname) == 'Linux' ]]; then
   PREFIX_DIR=$(readlink -f ${PREFIX_DIR})
 else
-  pushd ${PREFIX_DIR}
+  cd ${PREFIX_DIR}
   PREFIX_DIR=$(pwd -P)
-  popd
+  cd - &>/dev/null
 fi
 
 # Go one level up
