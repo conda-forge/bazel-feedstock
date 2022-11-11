dff977b06f178992d7332662aac3d15339c045ec
Uwe L. Korn <uwe.korn@quantco.com>
2022-11-11 10:30:43 +0100

    Fix JAVA_HOME

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -17,7 +17,7 @@ FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
 
 :: LIBRARY_PREFIX gets translated to '/' instead of the absolute path
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "JAVA_HOME=%%i/Library"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "JAVA_HOME=%%i/Library/lib/jvm"
 
 :: Need a very short TMPDIR otherwise we hit the max path limit while compiling bazel
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%SYSTEMDRIVE%\t"') DO set "TMPDIR=%%i"
