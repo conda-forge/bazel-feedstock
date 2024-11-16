b10a366d65aaed5c0a6b2f504b76aa74cc859411
Uwe L. Korn <uwe.korn@quantco.com>
2024-11-16 20:30:05 +0100

    Keep using crosstool

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,4 +6,4 @@ set -exuo pipefail
 cp -r ${RECIPE_DIR}/tutorial .
 cd tutorial
 source gen-bazel-toolchain
-bazel build --logging=6 --subcommands --verbose_failures //main:hello-world --platforms=//bazel_toolchain:target_platform --host_platform=//bazel_toolchain:build_platform --extra_toolchains=//bazel_toolchain:cc_cf_toolchain --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain
+bazel build --logging=6 --subcommands --verbose_failures //main:hello-world --crosstool_top=//bazel_toolchain:toolchain
