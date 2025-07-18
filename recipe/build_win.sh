1e6749e57c49ceaa30eafa8ad454eba512692ca4
Uwe L. Korn <uwe.korn@quantco.com>
2025-07-18 19:57:14 +0200

    Specify explicit output_base

--- recipe/build_win.sh
+++ recipe/build_win.sh
@@ -2,5 +2,6 @@
 
 set -euxo pipefail
 
-${SRC_DIR}/bazel-${PKG_VERSION}-windows-x86_64.exe build --cxxopt=/std:c++17 src:bazel_nojdk.exe --action_env=PATH --spawn_strategy=standalone --nojava_header_compilation --strategy=Javac=worker --worker_quit_after_build --ignore_unsupported_sandboxing --compilation_mode=opt --enable_bzlmod --check_direct_dependencies=error --lockfile_mode=update --subcommands --logging=6
+${SRC_DIR}/bazel-${PKG_VERSION}-windows-x86_64.exe --output_base=${SRC_DIR}/out build \
+	--cxxopt=/std:c++17 src:bazel_nojdk.exe --action_env=PATH --spawn_strategy=standalone --nojava_header_compilation --strategy=Javac=worker --worker_quit_after_build --ignore_unsupported_sandboxing --compilation_mode=opt --enable_bzlmod --check_direct_dependencies=error --lockfile_mode=update --subcommands --logging=6
 
