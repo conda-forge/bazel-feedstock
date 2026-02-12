12e545567a923927b9be14c2ea61045b500e9fc0
Uwe L. Korn <uwe.korn@quantco.com>
2026-02-12 19:50:20 +0100

    Update patches

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,6 +13,15 @@ fi
 # Generate toolchain and set necessary environment variables
 source gen-bazel-toolchain
 
+# Remove assemble actions from cxx_flags to avoid -stdlib=libc++ being passed
+# to assembly compilations, which causes -Werror failures (e.g. in BoringSSL)
+for _tc_cfg in bazel_toolchain/cc_toolchain_config.bzl bazel_toolchain/cc_toolchain_build_config.bzl; do
+  sed -i'' -e '/cxx_flags = feature/,/^    )/{
+    /ACTION_NAMES.assemble,/d
+    /ACTION_NAMES.preprocess_assemble,/d
+  }' "$_tc_cfg"
+done
+
 if [[ "${target_platform}" == "osx-64" ]]; then
   export TARGET_CPU="darwin"
 fi
@@ -30,8 +39,13 @@ export GRPC_VERSION=$(conda list -p $PREFIX libgrpc --fields version | grep -v '
 export PROTOC_VERSION=$(conda list -p $PREFIX libprotobuf | grep -v '^#' | tr -s ' ' | cut -f 2 -d ' ' | sed -E 's/^[0-9]+\.([0-9]+\.[0-9]+)$/\1/')
 export PROTOBUF_JAVA_MAJOR_VERSION="4"
 export BAZEL_BUILD_OPTS="--crosstool_top=//bazel_toolchain:toolchain --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include --cpu=${TARGET_CPU} --cxxopt=-std=c++17"
-export BAZEL_BUILD_OPTS="${BAZEL_BUILD_OPTS} --platforms=//bazel_toolchain:target_platform --host_platform=//bazel_toolchain:build_platform --extra_toolchains=//bazel_toolchain:cc_cf_toolchain --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain --toolchain_resolution_debug='.*'"
-export EXTRA_BAZEL_ARGS="--tool_java_runtime_version=21 --java_runtime_version=21"
+export BAZEL_BUILD_OPTS="${BAZEL_BUILD_OPTS} --platforms=//bazel_toolchain:target_platform --host_platform=//bazel_toolchain:build_platform --extra_toolchains=//bazel_toolchain:cc_cf_toolchain --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain --toolchain_resolution_debug='.*' --noincompatible_enable_proto_toolchain_resolution"
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  export BAZEL_BUILD_OPTS="${BAZEL_BUILD_OPTS} --macos_cpus=arm64 --@apple_support//platforms:macos_x86_64_platform=//bazel_toolchain:target_platform --@apple_support//platforms:macos_arm64_platform=//bazel_toolchain:target_platform"
+elif [[ "${target_platform}" == "osx-64" ]]; then
+  export BAZEL_BUILD_OPTS="${BAZEL_BUILD_OPTS} --macos_cpus=x86_64 --@apple_support//platforms:macos_x86_64_platform=//bazel_toolchain:target_platform --@apple_support//platforms:macos_arm64_platform=//bazel_toolchain:target_platform"
+fi
+export EXTRA_BAZEL_ARGS="--tool_java_runtime_version=21 --java_runtime_version=21 --toolchain_resolution_debug='@@bazel_tools//tools/cpp:toolchain_type'"
 sed -ie "s:PROTOC_VERSION:${PROTOC_VERSION}:" MODULE.bazel
 sed -ie "s:PROTOBUF_JAVA_MAJOR_VERSION:${PROTOBUF_JAVA_MAJOR_VERSION}:" MODULE.bazel
 sed -ie "s:PROTOC_VERSION:${PROTOC_VERSION}:" third_party/systemlibs/protobuf/MODULE.bazel
@@ -41,7 +55,11 @@ sed -ie "s:\${PREFIX}:${PREFIX}:" src/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/grpc/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/grpc-java/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/systemlibs/protobuf/BUILD
-sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/systemlibs/protobuf/src/google/protobuf/compiler/BUILD
+sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" \
+	third_party/systemlibs/protobuf/src/google/protobuf/compiler/BUILD \
+	third_party/systemlibs/protobuf/upb_generator/c/BUILD \
+	third_party/systemlibs/protobuf/upb_generator/minitable/BUILD \
+	third_party/systemlibs/protobuf/upb_generator/reflection/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/ijar/BUILD
 sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" src/tools/singlejar/BUILD
 sed -ie "s:TARGET_CPU:${TARGET_CPU}:" compile.sh
@@ -51,6 +69,17 @@ sed -ie "s:ABSEIL_VERSION:${ABSEIL_VERSION}:" MODULE.bazel
 sed -ie "s:GRPC_VERSION:${GRPC_VERSION}:" MODULE.bazel
 
 cp -ap $PREFIX/share/bazel/protobuf/bazel third_party/systemlibs/protobuf/
+
+# Bazel 9 removed native proto rules but kept stubs that fail; always use Starlark versions
+for bzl in third_party/systemlibs/protobuf/bazel/cc_proto_library.bzl \
+           third_party/systemlibs/protobuf/bazel/java_proto_library.bzl \
+           third_party/systemlibs/protobuf/bazel/java_lite_proto_library.bzl \
+           third_party/systemlibs/protobuf/bazel/proto_library.bzl; do
+  sed -ie 's/if not hasattr(native,.*/if True:/' "$bzl"
+done
+
+# rules_java 9 disallows deps without srcs in java_library; use runtime_deps instead
+#sed -ie '/runtime_deps/!s/deps = \["@maven_protobuf/runtime_deps = ["@maven_protobuf/' third_party/systemlibs/protobuf/BUILD
 
 ./compile.sh
 
@@ -78,7 +107,7 @@ pushd install-archive
 popd
 mv install-archive $PREFIX/share/bazel/install/${INSTALL_BASE_KEY}
 chmod -R a+w $PREFIX/share/bazel/install/${INSTALL_BASE_KEY}
-for executable in "build-runfiles" "daemonize" "linux-sandbox" "process-wrapper"; do
+for executable in "daemonize" "linux-sandbox" "process-wrapper"; do
   if [[ "${target_platform}" == osx-* ]]; then
     ${INSTALL_NAME_TOOL} -rpath ${PREFIX}/lib '@loader_path/../../../../lib' $PREFIX/share/bazel/install/${INSTALL_BASE_KEY}/$executable
   else
